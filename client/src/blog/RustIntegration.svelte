8c7b70966c05a734cd6dcd7606807a31b51cb4ac
Lowband <GraysonHieb21@Gmail.com>
2023-08-14 12:50:36 -0600

    blog post

---
+++ client/src/blog/RustIntegration.svelte
@@ -0,0 +1,86 @@
+<script>
+  console.log("RustIntegration Loaded");
+</script>
+
+<div class="post">
+  <h1>Blending Rust with Web Development: A Tale of Actix and Svelte</h1>
+
+  <h2>Diving into Rust</h2>
+  <p>
+    Rust, known for its impeccable memory safety while delivering peak
+    performance, has been a game-changer for many developers. From its rich type
+    system to its mastery over concurrency, Rust has become an invaluable asset
+    in my developer arsenal.
+  </p>
+
+  <h2>The First Experiment: Bringing 2048 to Life</h2>
+  <p>
+    My journey of merging Rust with web development commenced with recreating
+    the iconic 2048 game. The goal was to tap into Rust's efficiency, using its
+    logic to power a web-based rendition of the game, ensuring both fluidity and
+    speed.
+  </p>
+
+  <p>
+    The result was a highly responsive web-based game that capitalized on Rust's
+    reliability and speed, offering players a seamless experience.
+  </p>
+
+  <h2>Deepening the Fusion: Prime Number Generation with RSA</h2>
+  <p>
+    Building on the success of the 2048 integration, I tackled a more
+    computationally intensive challenge: prime number generation crucial for RSA
+    encryption. With Rust's computational muscle, the website witnessed a
+    transformative performance enhancement.
+  </p>
+
+  <p>
+    This integration was made possible with Actix, a Rust-powered web framework,
+    handling the backend intricacies. On the frontend, Svelte's reactive
+    paradigm smoothly interfaced with Actix, ensuring real-time updates and a
+    dynamic user experience.
+  </p>
+
+  <h2>Insights from the Integration</h2>
+  <p>
+    The harmonious combination of Rust with Actix and Svelte not only
+    supercharged the website's functionality but also exemplified Rust's
+    adaptability in the contemporary web development landscape.
+  </p>
+
+  <p>
+    This experience of converging Rust's backend prowess with Svelte's frontend
+    dynamism has illuminated new avenues in web development for me. It stands as
+    a testament to the endless possibilities when diverse technologies come
+    together in unison.
+  </p>
+
+  <h2>Conclusion</h2>
+  <p>
+    Integrating Rust, with the support of Actix, into a Svelte-driven web
+    environment has been a journey of discovery and innovation. It embodies the
+    limitless potential of modern web technologies. Reflecting on this
+    integration, I'm brimming with enthusiasm and excitement for the future. The
+    voyage of merging Rust with the expansive domain of web development is just
+    getting started, and the prospects are thrilling.
+  </p>
+</div>
+
+<style>
+  .post {
+    width: 90%;
+    margin: auto;
+    background-color: #333333;
+    margin-top: 20px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 5px;
+    border: 3px solid #000;
+  }
+  h1,
+  h2,
+  h3,
+  p {
+    color: #e8e8e8;
+  }
+</style>
--- client/src/blog/index.js
+++ client/src/blog/index.js
@@ -1,5 +1,6 @@
 import Post1 from './Post1.svelte';
 import Post2 from './Post2.svelte';
+import RustIntegration from './RustIntegration.svelte';
 
 export const blogPosts = [
   {
@@ -15,8 +16,14 @@ export const blogPosts = [
         title: "I take it back: Actix > Flask",
         brief: `Venture with me as I navigate my journey from Python's Flask to Rust's Actix. Experience the challenges and victories encountered as I master Rust's robust, efficient, and type-safe traits. This personal account will highlight my transition, focusing on setting up, routing, handling requests and responses, database connectivity, and error handling in Rust Actix. Despite a steep learning curve, I found Rust Actix's performance and robustness incredibly rewarding, enhancing my web development repertoire.`,
     }
+  },
+  {
+    component: RustIntegration,
+    metadata: {
+        title: "Integrating my Projects",
+        brief: `In this post, the author delves into the journey of incorporating their existing Rust projects into this portfolio website. Highlighting Rust's core strengths—memory safety, concurrency, and speed—the article details how the language was used to enhance the performance of mathematically intensive tasks for RSA encryption. Through the use of Actix and Svelte, prime generation was seamlessly integrated into the website, providing near-native performance and showcasing the vast possibilities when merging diverse tech stacks. The author concludes by reflecting on the enlightening experience and the future potential of such integrations in the ever-evolving web technology landscape.`,
+    }
   }
-  // Add more posts as needed
 ];
 
 export default blogPosts;
