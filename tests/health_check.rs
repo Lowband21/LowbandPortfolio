8d7372d7afe6d01ec9c6182b089cb664157c3b46
Lowband <GraysonHieb21@Gmail.com>
2023-08-20 12:11:15 -0600

    d

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,7 +3,7 @@ use actix_session::storage::RedisSessionStore;
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    //spawn_app().await;
+    let app_address = spawn_app().await;
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
@@ -21,7 +21,7 @@ async fn health_check_works() {
 #[tokio::test]
 async fn fractal_works() {
     // Arrange
-    //spawn_app().await;
+    let app_address = spawn_app().await;
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
@@ -42,13 +42,20 @@ async fn fractal_works() {
     assert_eq!(resp_body["height"].as_u64().unwrap(), 600);
 }
 
+use std::net::TcpListener;
+
 async fn spawn_app() {
+    let configuration = lowband_portfolio::configuration::get_configuration()
+        .expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random address");
     let redis_connection_string =
         std::env::var("REDIS_URL").unwrap_or_else(|_| String::from("redis://127.0.0.1:6379"));
     let redis_store = RedisSessionStore::new(redis_connection_string)
         .await
         .unwrap();
-    let server = lowband_portfolio::run(redis_store).expect("Failed to bind address");
+    let server = lowband_portfolio::startup::run(redis_store).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
