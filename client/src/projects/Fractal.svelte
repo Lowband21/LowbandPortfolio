1756058ac2481ff627e9571153bffca13f2c026d
Lowband <GraysonHieb21@Gmail.com>
2023-08-27 11:36:05 -0600

    major fractal update

--- client/src/projects/Fractal.svelte
+++ client/src/projects/Fractal.svelte
@@ -13,28 +13,30 @@
   // Constants
   const maxBaseIterations = 1000.0; // Base maximum iterations for Mandelbrot computation
   let maxIterations = maxBaseIterations;
-  const baseResolution = { width: 1200, height: 900 }; // Base resolution
+  const baseResolution = { width: 2800, height: 2100 }; // Base resolution
   let resolution = { ...baseResolution };
 
   // Function to adjust the maximum iterations based on the current zoom level
   function adjustMaxIterations() {
-    // Adjust the maximum iterations based on the zoom level, but do not exceed 25000
-    maxIterations = Math.min(maxBaseIterations / zoom, 25000);
+    if (zoom < 0.2) {
+      maxIterations = 20000.0;
+    } else if (zoom < 0.5) {
+      // Linear interpolation between 20000 and 5000 as zoom goes from 0.2 to 0.5
+      maxIterations = 20000.0 - (zoom - 0.2) * (15000.0 / 0.3);
+    } else {
+      // Linear interpolation between 5000 and 1000 as zoom goes from 0.5 to 1.0 (or more)
+      maxIterations = 5000.0 - (zoom - 0.5) * (4000.0 / 0.5);
+      maxIterations = Math.max(1000.0, maxIterations); // Ensure it doesn't go below 1000
+    }
   }
 
-  // Function to adjust the resolution based on the zoom level
-  function adjustResolution() {
-    let adjustedZoom = zoom < 0.5 ? zoom + 0.5 : 0.5;
-    resolution.width = baseResolution.width / adjustedZoom;
-    resolution.height = baseResolution.height / adjustedZoom;
-  }
-
+  let tessellationUpdateRequired = true;
   function getTessellationLevel(zoom, pan) {
     console.log(zoom);
     if (zoom < 0.2) return 128; // Use high tessellation for close-up views
     if (Math.abs(pan.x) < 0.5 && Math.abs(pan.y) < 0.5) return 64; // Higher tessellation near the center
-    if (zoom < 0.5) return 8;
-    if (zoom < 1.0) return 4;
+    if (zoom < 0.5) return 4;
+    if (zoom < 1.0) return 1;
     return 1; // Lowest tessellation for distant views
   }
 
@@ -67,7 +69,8 @@
   // Vertex Shader Source
   // This shader simply passes the vertex positions to the fragment shader
   const vs = `
-    attribute vec2 position;
+    #version 300 es
+    in vec2 position;
 
     void main() {
     gl_Position = vec4(position, 0.0, 1.0);
@@ -77,56 +80,74 @@
   // Fragment Shader Source for Mandelbrot
   // This shader computes the Mandelbrot set and assigns colors based on the iteration count
   const fs = `
-    precision mediump float; // Set the precision for floating point numbers
-
+    #version 300 es
+    precision mediump float;
+    out vec4 outputColor;
     const float TWO = 2.0;
     const float FOUR = 4.0;
+    const float COLOR_MULTIPLIER = 90.0;
+    uniform vec2 resolution;
+    uniform vec2 pan;
+    uniform float zoom;
+    uniform float maxIterations;
+    
+vec3 getColor(float iterations) {
+    vec3 color;
+    float normalized = pow(iterations / maxIterations, 0.3); // Adjust the exponent to tweak the sensitivity
 
-    uniform vec2 resolution; // Screen resolution
-    uniform vec2 pan; // Panning offset
-    uniform float zoom; // Zoom level
-    uniform float maxIterations; // Maximum iterations for Mandelbrot computation
+    // Define thresholds for the color bands
+    float t1 = 0.1;  
+    float t2 = 0.3;  
+    float t3 = 0.6;  
+    float t4 = 0.8;  
 
-    // Function to get the color based on the iteration count
-    vec3 getColor(float iterations) {
-        vec3 color;
-        float normalized = iterations / maxIterations;
-        color.r = mix(0.4, 1.0, sin(90.0 * normalized));
-        color.g = mix(0.3, 0.5, sin(90.0 * normalized + 4.0));
-        color.b = mix(0.5, 1.0, sin(90.0 * normalized + 2.0));
-        return color;
+    if (normalized < t1) {
+        color = mix(vec3(1.0, 1.0, 1.0), vec3(0.0, 0.0, 1.0), normalized / t1);
+    } else if (normalized < t2) {
+        color = mix(vec3(0.0, 0.0, 1.0), vec3(1.0, 0.0, 0.0), (normalized - t1) / (t2 - t1));
+    } else if (normalized < t3) {
+        color = mix(vec3(1.0, 0.0, 0.0), vec3(1.0, 0.5, 0.0), (normalized - t2) / (t3 - t2));
+    } else if (normalized < t4) {
+        color = mix(vec3(1.0, 0.5, 0.0), vec3(0.0, 0.0, 0.0), (normalized - t3) / (t4 - t3));
+    } else {
+        color = vec3(0.0, 0.0, 0.0);
     }
 
+    return color;
+}
+    
     void main() {
-        // Calculate the normalized screen coordinates
         vec2 uv = (gl_FragCoord.xy - 0.5 * resolution) / min(resolution.y, resolution.x);
         uv *= zoom;
         uv += pan;
-    
-        // Initialize Mandelbrot variables
         vec2 c = uv;
         vec2 z = vec2(0.0);
         float iterations = 0.0;
-    
-        // Update loop condition
-        for (float i = 0.0; i < 25000.0; i++) {
+        for (float i = 0.0; i < maxIterations; i++) {
             if (dot(z, z) > FOUR) break;
-
             vec2 temp = vec2(z.x*z.x - z.y*z.y, TWO * z.x*z.y) + c;
             z = temp;
             iterations++;
         }
-    
-        // Get the color based on the iteration count and set the fragment color
         vec3 color = getColor(iterations);
-        gl_FragColor = vec4(color, 1.0);
+        outputColor = vec4(color, 1.0);
     }
 `;
+  let debounceTimeout;
 
+  function debouncedRender() {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+      adjustMaxIterations();
+      tessellationUpdateRequired = true;
+    }, 100);
+  }
   // Svelte lifecycle function that runs when the component is mounted
   onMount(async () => {
     // Get the WebGL context from the canvas element
-    const gl = document.getElementById("canvas").getContext("webgl");
+    const gl = document
+      .getElementById("canvas")
+      .getContext("webgl2", { antialias: true });
 
     // Create a WebGL program using the provided vertex and fragment shaders
     const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
@@ -135,8 +156,6 @@
     const arrays = {
       position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
     };
-    // Create a buffer with the vertex positions
-    const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
 
     let previousTessellationLevel = -1;
     let tessellatedBufferInfo; // Declare it outside of the render function
@@ -166,8 +185,7 @@
 
       // Determine the tessellation level based on zoom and generate the tessellated quads
       const tessellationLevel = getTessellationLevel(zoom, pan);
-      // Only update the tessellated buffer if the tessellation level changes
-      if (tessellationLevel !== previousTessellationLevel) {
+      if (tessellationUpdateRequired) {
         const quads = createTessellatedQuads(tessellationLevel);
         const arrays = {
           position: {
@@ -175,8 +193,8 @@
             data: quads,
           },
         };
-        tessellatedBufferInfo = twgl.createBufferInfoFromArrays(gl, arrays); // Update the buffer info
-        previousTessellationLevel = tessellationLevel;
+        tessellatedBufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+        tessellationUpdateRequired = false;
       }
 
       // Bind the WebGL program and set the buffers, attributes, and uniforms
@@ -195,7 +213,7 @@
     function handleWheel(event) {
       const delta = event.deltaY;
       zoom *= delta > 0 ? 1.1 : 0.9;
-      adjustResolution(); // Adjust the resolution when the zoom level changes
+      debouncedRender();
     }
 
     // Function to handle panning with the mouse
@@ -224,6 +242,11 @@
 
     // Start the rendering loop
     requestAnimationFrame(render);
+    return () => {
+      canvas.removeEventListener("wheel", handleWheel);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("wheel", preventScroll);
+    };
   });
 </script>
 
