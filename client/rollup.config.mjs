10da49c2bd46f94e350014a35f4cdfdbde0855aa
Grayson Hieb <graysonhieb21@gmail.com>
2024-04-23 11:37:31 -0600

    remove commonjs plugin

--- client/rollup.config.mjs
+++ client/rollup.config.mjs
@@ -1,6 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import terser from '@rollup/plugin-terser';
 import css from 'rollup-plugin-css-only';
@@ -41,7 +40,6 @@ export default {
 			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/'),
 		  exportConditions: ['svelte'],
 		}),
-		commonjs(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
