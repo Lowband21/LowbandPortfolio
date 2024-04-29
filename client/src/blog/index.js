50ab80cebf46e17e22a64d0fbb38a00722d2fd5d
Grayson Hieb <graysonhieb21@gmail.com>
2024-04-29 13:44:13 -0600

    blog update

--- client/src/blog/index.js
+++ client/src/blog/index.js
@@ -6,15 +6,8 @@ export const blogPosts = [
   {
     component: Post1,
     metadata: {
-        title: "Unleashing the Power of Full Stack Development with Svelte and Flask",
-        brief: "This website was written with a combination of Svelte and Flask, this this how it went."
-    }
-  },
-  {
-    component: Post2,
-    metadata: {
-        title: "I take it back: Actix > Flask",
-        brief: `Venture with me as I navigate my journey from Python's Flask to Rust's Actix. Experience the challenges and victories encountered as I master Rust's robust, efficient, and type-safe traits. This personal account will highlight my transition, focusing on setting up, routing, handling requests and responses, database connectivity, and error handling in Rust Actix. Despite a steep learning curve, I found Rust Actix's performance and robustness incredibly rewarding, enhancing my web development repertoire.`,
+        title: "Building My Portfolio Website",
+        brief: "This website was written with a combination of Svelte and Actix, this this how it went."
     }
   },
   {
